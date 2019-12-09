5de3bbe28d17d9a96651d9a546c8587d37428b59
Jonathan Moscovici <jonmosco@gmail.com>
2019-12-09 09:02:22 -0500

    initial commit

---
+++ setup.R
@@ -0,0 +1,17 @@
+library(blogdown)
+setwd("H:/Programming/blogdown/jonathanmoscovici/jonathanmoscovici/jonathanmoscovici")
+#blogdown::new_site(theme="gcushen/hugo-academic", theme_example=TRUE)
+
+
+blogdown::new_site(dir="Theme")
+
+#code to install new theme:
+
+#library(blogdown)
+#new_site() # default theme is lithium
+# need to stop serving so can use the console again
+#install_theme("gcushen/hugo-academic", theme_example = TRUE, update_config = TRUE)
+
+
+#make sure hudo version on PC matches version on Netlify
+blogdown::hugo_version()
